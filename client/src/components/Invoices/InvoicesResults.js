d1dd8a8d8f0ea625b76ba28787e0165269392f47
The Mai <Mai.The.09@gmail.com>
2018-07-17 12:55:53 -0500

    remove placeholder text from invoices view

--- client/src/components/Invoices/InvoicesResults.js
+++ client/src/components/Invoices/InvoicesResults.js
@@ -6,19 +6,19 @@ export class InvoicesResults extends Component {
 		const invoices = this.props.invoices;
 		return (
 			<div className="invoices col s8">
-				<div className="col s12 z-depth-2">
-					<div className="divider"></div>
-					<div className="invoices-results">
-						{(invoices.length > 0)
-							? this.props.invoices.map((invoice, i) => {
+				{(invoices.length > 0)
+					? < div className="col s12 z-depth-2">
+						<div className="divider"></div>
+						<div className="invoices-results">
+							{this.props.invoices.map((invoice, i) => {
 								return <InvoicesResultsRow key={i} invoice={invoice} />
-							})
-							: <div className="placeholder center-align valign-wrapper trans-card">
-								<h2>Hey dawg make this look nice</h2>
-							</div>
-						}
+							})}
+						</div>
 					</div>
-				</div>
+					: <div className="placeholder center-align valign-wrapper trans-card">
+						<h2></h2>
+					</div>
+				}
 			</div>
 		)
 	};

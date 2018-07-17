618ecdd88d2efaabc62ec3ad2d6c975d600d2714
The Mai <Mai.The.09@gmail.com>
2018-07-17 16:07:21 -0500

    add student info/enrollment to student detail view

--- client/src/components/TabDetails/StudentDetails.js
+++ client/src/components/TabDetails/StudentDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './StudentDetails.css';
 import $ from 'jquery';
 
 export class StudentDetails extends Component {
@@ -14,10 +15,11 @@ export class StudentDetails extends Component {
 		length: '',
 		initFee: '',
 		rateFee: '',
-		uniform: false,
 		parID: '',
 		parIDtwo: '',
-		parName: 'Please enter Parent ID',
+		parName: '',
+		expDate: '',
+		willRenew: true,
 	}
 
 	dojoArr = [
@@ -63,6 +65,29 @@ export class StudentDetails extends Component {
 		$('#length').on('change', this.handleChangeDropdown);
 	}
 
+	handleChange = (e) => {
+		this.setState({ [e.target.id]: e.target.value });
+	}
+
+	handleChangeCheckbox = (e) => {
+		this.setState({ [e.target.id]: (e.target.value === 'false') ? 'true' : 'false' });
+		setTimeout(() => { console.log(this.state) }, 10);
+	}
+
+	handleBlur = (e) => {
+		if (e.target.value !== ''); $(`#${e.target.id}`).removeClass('invalid'); $(`#${e.target.id}`).addClass('valid');
+	}
+
+	handleBlurNumber = (e) => {
+		let rate = parseFloat(e.target.value).toFixed(2);
+		this.setState({ [e.target.id]: rate });
+		setTimeout(() => { console.log(this.state) }, 1)
+	}
+
+	handleSubmit = (e) => {
+		window.Materialize.toast(`Sorry! This function not yet implemented`, 5000, 'animated bounceInUp red darken-2');
+	}
+
 	allRequiredFilled = () => {
 		// Helper function to detect when animation ends
 		const animationEnd = (function (el) {
@@ -99,7 +124,140 @@ export class StudentDetails extends Component {
 		const data = this.props.data;
 		return (
 			<div id={`${data.idtwo}`} className="detail-row row">
-				Student {data.info.name.dFull}
+				<div className="col s7">
+					<div className="addstudent-form">
+						<form className="detail-info col s12 z-depth-2 trans-card" action="#">
+							<div className="student-info row">
+								<h5 className="col s12">Student Info</h5>
+								<div className="input-field col s12 m6">
+									<input
+										id="first" type="text" className="required validate"
+										value={this.state.first} onChange={this.handleChange}
+									/>
+									<label htmlFor="first">First Name *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<input
+										id="last" type="text" className="required validate"
+										value={this.state.last} onChange={this.handleChange}
+									/>
+									<label htmlFor="last">Last Name *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<input
+										id="school" type="text" className="validate"
+										value={this.state.school} onChange={this.handleChange}
+									/>
+									<label htmlFor="school">Public School Attending</label>
+								</div>
+								<div className="input-field col s12 m3">
+									<input
+										id="phone" type="tel" className="validate"
+										value={this.state.phone} onChange={this.handleChange}
+									/>
+									<label htmlFor="phone">Phone Number</label>
+								</div>
+								<div className="input-field col s12 m3">
+									<input
+										id="dob" type="date" className="required datepicker validate"
+										value={this.state.dob} onChange={this.handleChange} onBlur={this.handleBlur}
+									/>
+									<label htmlFor="dob">Date of Birth *</label>
+								</div>
+							</div>
+							<div className="submit-btn center-align">
+								<a className="waves-effect waves-light btn-large" onClick={this.handleSubmit}>Update<i className="material-icons right">person_add</i></a>
+							</div>
+						</form>
+					</div>
+					<div className="addstudent-form">
+						<form className="detail-enrollment col s12 z-depth-2 trans-card" action="#">
+							<div className="enrollment-info row">
+								<h5 className="col s12">Tuition/Enrollment Info</h5>
+								<div className="input-field col s12 m6">
+									<select
+										id="dojo" type="text" className="required validate"
+										value={this.state.dojo} onChange={this.handleChange}>
+										<option key="0" value="" disabled>Select Dojo</option>
+										{this.dojoArr.map((dojo, i) => {
+											return <option key={i + 1} value={dojo}>{dojo}</option>
+										})}
+									</select>
+									<label htmlFor="dojo">Dojo *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<select
+										id="beltRank" type="text" className="required validate"
+										value={this.state.beltRank} onChange={this.handleChange}>
+										<option key="0" value="" disabled>Select Belt Rank</option>
+										{this.beltRankArr.map((beltRank, i) => {
+											return <option key={i + 1} value={beltRank}>{beltRank}</option>
+										})}
+									</select>
+									<label htmlFor="beltRank">Belt Rank *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<select
+										id="type" type="text" className="required validate"
+										value={this.state.type} onChange={this.handleChange}>
+										<option key="0" value="" disabled>Select Program Type</option>
+										{this.enrollmentTypeArr.map((type, i) => {
+											return <option key={i + 1} value={type}>{type}</option>
+										})}
+									</select>
+									<label htmlFor="type">Program Type *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<select
+										id="length" type="text" className="required validate"
+										value={this.state.length} onChange={this.handleChange}>
+										<option key="0" value="" disabled>Select Program Length</option>
+										{this.enrollmentLengthArr.map((length, i) => {
+											return <option key={i + 1} value={length}>{length}</option>
+										})}
+									</select>
+									<label htmlFor="length">Program Length *</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<input
+										id="expDate" type="date" className="required datepicker validate"
+										value={this.state.expDate} onChange={this.handleChange} onBlur={this.handleBlur}
+									/>
+									<label htmlFor="expDate">Start Date</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<input
+										id="expDate" type="date" className="required datepicker validate"
+										value={this.state.expDate} onChange={this.handleChange} onBlur={this.handleBlur}
+									/>
+									<label htmlFor="expDate">Expiration Date</label>
+								</div>
+								<div className="input-field col s12 m6">
+									<input
+										id="rateFee" type="number" className="required validate"
+										value={this.state.rateFee} onChange={this.handleChange} onBlur={this.handleBlurNumber}
+									/>
+									<label htmlFor="rateFee">Renewal Fee ($) *</label>
+								</div>
+								<div className="input-field check-box-field col s12 m6">
+									<p>
+										<label htmlFor="willRenew">
+											<input id="willRenew" type="checkbox" className="filled-in"
+												value={this.state.willRenew} onChange={this.handleChangeCheckbox}
+											/>
+											<span>Automatic Renewal</span>
+										</label>
+									</p>
+								</div>
+							</div>
+							<div className="submit-btn center-align">
+								<a className="waves-effect waves-light btn-large" onClick={this.handleSubmit}>Update<i className="material-icons right">person_add</i></a>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div>
+				</div>
 			</div>
 		)
 	}

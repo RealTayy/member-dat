2ea46bf5737d0b088a43801c68c8af0245433ebf
TheMaiEQX <mai.the.09@gmail.com>
2018-10-07 15:31:28 -0500

    style header. add collpase button

--- client/src/components/Dashboard/DashNewsCard.js
+++ client/src/components/Dashboard/DashNewsCard.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import './DashNewsCard.css';
+import $ from 'jquery';
 
 export class DashNewsCard extends Component {
+  handleCollapse(e) {	  
+	  const icon = $(e.target).children('.material-icons');
+	  //if ()
+	  icon.text('expand_more');
+  }
+  
   render() {
     return (
-      <div className="news-card col s12 trans-card z-depth-2">
-        <div className="news-header">I am a Header</div>
-        <div className="news-contect">I am some Content</div>
+      <div className="news-card col s12 trans-card z-depth-2 expanded">
+        <div className="news-header blue darken-1">
+		  <div className="news-header-text col">
+		    Info about Demo mode
+		  </div>
+		  <div className="news-header-collapse right waves-effect waves-light" onClick={this.handleCollapse}>
+		    <i className="material-icons">expand_less</i>
+		  </div>
+		</div>
+        <div className="news-context row">I am some Content</div>
       </div>
     )
   }
 }
 
-export default DashNewsCard
+export default DashNewsCard;

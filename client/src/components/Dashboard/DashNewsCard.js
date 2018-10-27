06209c7cfea3b0839ffb4ede7b1a842c0c6afaee
TheMaiEQX <mai.the.09@gmail.com>
2018-10-26 21:07:49 -0500

    add collapse effect

--- client/src/components/Dashboard/DashNewsCard.js
+++ client/src/components/Dashboard/DashNewsCard.js
@@ -4,28 +4,44 @@ import $ from 'jquery';
 
 export class DashNewsCard extends Component {
   handleCollapse(e) {
-    const $header = $(e.target).parent();
-    if ($header.hasClass('expanded')) {
-      $header.removeClass('expanded');
-      $header.addClass('collapsed');
+    const $card = $(e.target).parent().parent();
+	const $header = $(e.target).parent();
+	const $context = $card.find('.news-context');
+	const initHeight = ($context[0].scrollHeight);$context.height(`${initHeight}px`);
+    if ($card.hasClass('expanded')) {
+      $card.removeClass('expanded');
+      $card.addClass('collapsed');
+	  $context.height('0px');
     } else {
-      $header.removeClass('collapsed');
-      $header.addClass('expanded');
-    }
+      $card.removeClass('collapsed');
+      $card.addClass('expanded');
+	  $context.height(`${initHeight}px`);
+    }		
   }
 
   render() {
     return (
       <div className="news-card col s12 trans-card z-depth-2 expanded">
-        <div className="news-header blue darken-1 expanded">
+        <div className="news-header blue darken-1">
           <div className="news-header-text col">
             Upcoming Features
-		      </div>
+		  </div>
           <div className="news-header-collapse right waves-effect waves-light" onClick={this.handleCollapse}>
             <i className="material-icons"></i>
           </div>
         </div>
-        <div className="news-context ">1. This Dashboard's functionality</div>
+        <div className="news-context ">
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+			<div>1. This Dashboard's functionality</div>
+		</div>
       </div>
     )
   }
